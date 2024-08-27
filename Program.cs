b94b8cae510cc0a11cdd7906bbcf202a4ffaa93b
hektasBarbaros <barbaros.sevgen@outlook.com.tr>
2024-08-27 15:47:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace qr_code_generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://documents.hektas.com.tr/";
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20); // 20 piksel genişlikli modüller
+
+            string filePath = "C:\\Users\\bsevgen\\source\\repos\\qr_code_generator\\repo\\QRCode.png";
+
+            string directoryPath = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            try
+            {
+                qrCodeImage.Save(filePath, ImageFormat.Png);
+                Console.WriteLine($"QR kodu '{filePath}' dosyasına kaydedildi.");
+            }
+            catch (ExternalException ex)
+            {
+                Console.WriteLine($"Hata: {ex.Message}");
+            }
+        }
+    }
+}
